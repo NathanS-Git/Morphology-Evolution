0eddbe075eb61cd9fcfde031ac2bb2eebd3aed2d
Nathan <nathan.smith.compsci@gmail.com>
2022-04-21 13:50:32 -0400

    Removed testing code

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ if (__name__ == '__main__'):
             try:
                 #fitness.append(np.random.randint(-1000,1000))
                 #fitness.append(eval_morphology(file_path,gen*episode_increase_per_gen))
-                #fitness.append(eval_morphology(file_path,static_eps_eval))
-                fitness.append(np.random.random())
+                fitness.append(eval_morphology(file_path,static_eps_eval))
+                #fitness.append(np.random.random())
             except Exception:
                 print("Bad morphology. Something went wrong.")
                 traceback.print_exc()
